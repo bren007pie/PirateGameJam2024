78299f59d936a9326c2068419dc00be1ce7ed47d
FunkyMushrooms <162249908+FunkyMushrooms@users.noreply.github.com>
2024-07-26 22:35:02 -0400

    Added look at

--- shabah.gd
+++ shabah.gd
@@ -2,12 +2,13 @@ extends CharacterBody2D
 
 const ACCL = 900.0
 const MAX_SPEED = 300.0
+@onready var ray_cast_2d = $RayCast2D
+@onready var sprite_2d = $Sprite2D
 
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	
 	var direction = Vector2(Input.get_axis("Left", "Right"), Input.get_axis("Up", "Down"))
 	if direction.length_squared() == 0:
@@ -17,5 +18,10 @@ func _physics_process(delta):
 		
 	if velocity.length() > MAX_SPEED:
 		velocity = velocity.normalized()*MAX_SPEED
-
+	
+	if velocity.length():
+		sprite_2d.rotation = velocity.angle() - deg_to_rad(90)
+	
+	ray_cast_2d.rotation = sprite_2d.rotation
+	
 	move_and_slide()
